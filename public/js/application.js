8f258e329577d5d682e93648325ac2b8f338fe3a
Duane Johnson <djohnson@getcollc.com>
2011-04-09 10:36:49 -0500

    Added form, removed connect submodule

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,50 @@
 new Ext.Application({
     launch: function() {
-        new Ext.Panel({
-            fullscreen: true,
-            html: 'Hello World!'
-        });
+        // new Ext.Panel({
+        //     fullscreen: true,
+        //     html: 'Hello World!'
+        // });
+        var formBase = {
+            scroll: 'vertical',
+            url   : 'postUser.php',
+            standardSubmit : false,
+            items: [{
+                    xtype: 'fieldset',
+                    title: 'Personal Info',
+                    instructions: 'Please enter the information above.',
+                    defaults: {
+                        required: true,
+                        labelAlign: 'left',
+                        labelWidth: '40%'
+                    },
+                    items: [
+                    {
+                        xtype: 'textfield',
+                        name : 'name',
+                        label: 'Name',
+                        useClearIcon: true,
+                        autoCapitalize : false
+                    }]
+			}]
+		}
+		
+		if (Ext.is.Phone) {
+            formBase.fullscreen = true;
+        } else {
+            Ext.apply(formBase, {
+                autoRender: true,
+                floating: true,
+                modal: true,
+                centered: true,
+                hideOnMaskTap: false,
+                height: 385,
+                width: 480
+            });
+        }
+        
+        form = new Ext.form.FormPanel(formBase);
+        form.show();
+
+
     }
 });
