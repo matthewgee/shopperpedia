61af75bf72cdb533149d38acd9f734f44d4d5d45
Duane Johnson <djohnson@getcollc.com>
2011-04-09 17:50:26 -0500

    Add browserify npm module

--- public/js/application.js
+++ public/js/application.js
@@ -1,50 +1,107 @@
 new Ext.Application({
+    name: 'Shopperpedia',
+
     launch: function() {
-        // new Ext.Panel({
-        //     fullscreen: true,
-        //     html: 'Hello World!'
-        // });
-        var formBase = {
-            scroll: 'vertical',
-            url   : 'postUser.php',
-            standardSubmit : false,
-            items: [{
-                    xtype: 'fieldset',
-                    title: 'Personal Info',
-                    instructions: 'Please enter the information above.',
+        this.viewport = new Ext.Panel({
+            fullscreen: true,
+
+            id    : 'mainPanel',
+            layout: 'card',
+            items : [
+                {
+                    xtype: 'carousel',
                     defaults: {
-                        required: true,
-                        labelAlign: 'left',
-                        labelWidth: '40%'
+                        cls: 'card'
                     },
                     items: [
-                    {
-                        xtype: 'textfield',
-                        name : 'name',
-                        label: 'Name',
-                        useClearIcon: true,
-                        autoCapitalize : false
-                    }]
-			}]
-		}
-		
-		if (Ext.is.Phone) {
-            formBase.fullscreen = true;
-        } else {
-            Ext.apply(formBase, {
-                autoRender: true,
-                floating: true,
-                modal: true,
-                centered: true,
-                hideOnMaskTap: false,
-                height: 385,
-                width: 480
-            });
-        }
+                        {
+                            html: ''
+                        },
+                        {
+                            title: 'Tab 2',
+                            html: '2'
+                        },
+                        {
+                            title: 'Tab 3',
+                            html: '3'
+                        }
+                    ]
+                }
+            ]
+        });
+    }
+});
+
+
+new Ext.Application({
+    launch: function() {
         
-        form = new Ext.form.FormPanel(formBase);
-        form.show();
+        // var formBase = {
+        //     scroll: 'vertical',
+        //     url   : 'postUser.php',
+        //     standardSubmit : false,
+        //     items: [{
+        //             xtype: 'fieldset',
+        //             title: 'Personal Info',
+        //             // instructions: 'Please enter the information above.',
+        //             defaults: {
+        //                 required: true,
+        //                 labelAlign: 'left',
+        //                 labelWidth: '40%'
+        //             },
+        //             items: [
+        //             {
+        //                 xtype: 'textfield',
+        //                 name : 'name',
+        //                 label: 'Name',
+        //                 useClearIcon: true,
+        //                 autoCapitalize : true
+        //             },
+        //             {
+        //                 xtype: 'textfield',
+        //                 name : 'email',
+        //                 label: 'Email',
+        //                 useClearIcon: true,
+        //                 autoCapitalize : false
+        //             }
+        //             ]
+        //     },
+        //     {
+        //         xtype: 'button',
+        //         text: 'Next',
+        //         // align: 'right',
+        //         width: '50%',
+        //         ui: 'forward',
+        //         handler: function() {
+        //             if(formBase.user){
+        //                 form.updateRecord(formBase.user, true);
+        //             }
+        //             form.submit({
+        //                 waitMsg : {message:'Submitting', cls : 'demos-loading'}
+        //             });
+        //         }
+        //     }],
+        // }
+        // 
+        // if (Ext.is.Phone) {
+        //     formBase.fullscreen = true;
+        // } else {
+        //     Ext.apply(formBase, {
+        //         autoRender: true,
+        //         floating: true,
+        //         modal: true,
+        //         centered: true,
+        //         hideOnMaskTap: false,
+        //         height: 385,
+        //         width: 480
+        //     });
+        // }
+        // 
+        // form = new Ext.form.FormPanel(formBase);
+        // form.show();
 
-
+        // var carousel1 = new Ext.Carousel();
+        // 
+        // carousel1.show();
     }
 });
